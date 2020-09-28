d2604ea97340aa7a0fb3a22ea0070b72cd5a659f
luuksg12 <0966746@hr.nl>
2020-09-28 17:22:38 +0200

    Added basic css and images.

--- DiscussIt_Website/resources/views/layouts/postLayout.blade.php
+++ DiscussIt_Website/resources/views/layouts/postLayout.blade.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<link href="{{ asset('css/main.css') }}" rel="stylesheet">
 <head>
     <title>DiscussIt</title>
 </head>
 
-<body class="antialiased">
+<div>
+<img src="/img/logoprog.png" width="80px" height="80px">
 @yield('title')
-<nav>
-<a href="/">home</a> <a href="/post">post</a>
-</nav>
+</div>
+
+<body class="antialiased">
+    <nav>
+        <a href="/">home</a> <a href="/post">post</a>
+    </nav>
+
 @yield('post')
 </body>
 </html>
--- DiscussIt_Website/resources/views/post.blade.php
+++ DiscussIt_Website/resources/views/post.blade.php
@@ -3,11 +3,13 @@
             <h1>Posts</h1>
         @endsection
         @section("post")
-        <div>
+        <div id="postholder">
             @foreach($posts as $post)
+                <div id="post">
                 <h2>Title : {{$post['title']}}</h2>
-                <h4>Post number : {{$loop->index + 1}} Author : -author-</h4>
+                <h4>Post number : {{$loop->index + 1}},  Author : -author-</h4>
                 <p>Message : {{$post['text']}}</p>
+                </div>
             @endforeach
         </div>
         @endsection
