dd11644214891be22922862ec3e0a1b4e2cbbca2
luuksg12 <0966746@hr.nl>
2020-10-05 11:21:09 +0200

    added a database using MYSQL, Learned how to use controllers to get data from them, Learned how to use models, Learned about migrations. And did all of it

---
+++ DiscussIt_Website/app/Http/Controllers/PostController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function databaseLoad(){
+
+    }
+
+    public function index(){
+
+        $posts = Post::all();
+
+        return view('post',['posts' => $posts]);
+    }
+    public function show($id){
+        $postsarray = [
+            ['title'=>'I am in love with laravel', 'text'=>'It is just so cool!!! I cant get enough of it :D'],
+            ['title'=>'What about aliens?', 'text'=>'They might really exist if you think about it?!'],
+            ['title'=>'Should pineapple on pizza be illegal?', 'text'=>'Like honestly its probably the most unsettling thing humans have ever done...'],
+            ['title'=>'Why cant you cycle on they highway?????', 'text'=>'I can cycle about 130km/h if I try. So why cant I cycle on the highway?']
+        ];
+        return view('singlepost',['post' => $postsarray,'id'=>$id]);
+    }
+}
--- DiscussIt_Website/resources/views/post.blade.php
+++ DiscussIt_Website/resources/views/post.blade.php
@@ -6,9 +6,10 @@
         <div id="postholder">
             @foreach($posts as $post)
                 <div id="post">
-                <h2>Title : {{$post['title']}}</h2>
-                <h4>Post number : {{$loop->index + 1}},  Author : {{$author}}</h4>
-                <p>Message : {{$post['text']}}</p>
+                <h2>Title : {{$post->title}}</h2>
+                <h4>Post number : {{$post->id}} <br>
+                    Author : {{$post->author}}</h4>
+                <p>Message : {{$post->text}}</p>
                 </div>
             @endforeach
         </div>
