a8c1601ef96d8bd56316fc63a910608d45378c5a
luuksg12 <0966746@hr.nl>
2020-10-24 15:57:59 +0200

    Made my own role system and added administration

--- DiscussIt_Website/app/Http/Controllers/PostController.php
+++ DiscussIt_Website/app/Http/Controllers/PostController.php
@@ -20,9 +20,12 @@ class PostController extends Controller
         }else{
             $admin = false;
         }
+
+        $name = auth()->user()->name;
+
         $posts = Post::all();
 
-        return view('post',['posts' => $posts, 'admin' => $admin]);
+        return view('post',['posts' => $posts, 'admin' => $admin, 'name' => $name]);
     }
     public function show($id){
         $post = Post::findOrFail($id);
