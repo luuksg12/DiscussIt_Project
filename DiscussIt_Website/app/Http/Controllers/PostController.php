577475c7a118df9eadcbbd27cf6da3a4722f4e46
luuksg12 <0966746@hr.nl>
2020-11-01 16:58:56 +0100

    Removed if-check from blade and only gave the correct data from the controller

--- DiscussIt_Website/app/Http/Controllers/PostController.php
+++ DiscussIt_Website/app/Http/Controllers/PostController.php
@@ -31,7 +31,7 @@ class PostController extends Controller
         return view('admin',['post' => $posts]);
     }
     public function myposts(){
-        $posts = Post::all();
+        $posts = DB::table('posts')->WHERE('author','=', auth()->user()->name)->get();
         return view('myposts',['posts' => $posts]);
     }
     public function create(){
